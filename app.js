442e507f0051ba5094ada67de918e952c7bb19ec
tomlikescode <gtmallen@gmail.com>
2021-10-07 20:18:39 -0500

    10-7-21 2

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const projects = [
     {
         id: 1,
-
+        img: './images/media.png',
+        title: 'pest control website',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero consequuntur quas dolor nulla fugiat facilis fuga nihil harum nam cumque vel, deserunt veniam! Fuga error iste, earum quos similique temporibus!'
+        languageA: './assets/html5-brands.svg',
+        languageB: './assets/css3-alt-brands.svg',
+        languageC: './assets/js-brands.svg',
+        liveSiteURL: '#',
+        githubURL: '#',
     },
     {
         id: 2,
+        img: './images/media.png',
+        title: 'restaurant website',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero consequuntur quas dolor nulla fugiat facilis fuga nihil harum nam cumque vel, deserunt veniam! Fuga error iste, earum quos similique temporibus!'
+        languageA: './assets/html5-brands.svg',
+        languageB: './assets/sass-brands.svg',
+        languageC: './assets/js-brands.svg',
+        liveSiteURL: '#',
+        githubURL: '#',
     },
 ];
