6f3cec3aec629a2e6a473610533065b3d99e2aa3
tomlikescode <gtmallen@gmail.com>
2021-10-16 08:06:10 -0500

    push

--- app.js
+++ app.js
@@ -22,3 +22,40 @@ const projects = [
         githubURL: '#',
     },
 ];
+
+const sectionCenter = document.querySelector('.project-wrapper');
+
+window.addEventListener('DOMContentLoaded', ()=> {
+    displayProjects(projects);
+})
+
+function displayProjects (projectItems) {
+    let displayMenu = projectItems.map((item)=>{
+
+        return `<div class="project1 project-unit" id="project-specific">
+                    <img src=${item.img} id="media-formats" alt="${item.title}">
+                    <h2 class="project-name">
+                        ${item.title}
+                    </h2>
+                    <p class="project-description">
+                        ${item.description}
+                    </p>
+                    <div class="made-with">
+                        <div class="made-with-title">
+                            <h3>made with</h3>
+                        </div>
+                        <div class="languages">
+                            <img src=${item.languageA} alt="">
+                            <img src=${item.languageB} alt="">
+                            <img src=${item.languageC} alt="">
+                        </div>
+                    </div>
+                    <div class="project-links">
+                        <a href=${item.liveSiteURL}>live site</a>
+                        <div class="vertical-line"></div>
+                        <a href=${item.githubURL}>github</a>
+                    </div>
+                
+                </div>`
+    })
+}
